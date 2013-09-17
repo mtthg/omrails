b1438a5e941312bae7460b4202c3e03e306be13e
Matthew Huang <matt8huang@gmail.com>
2013-09-16 21:45:09 -0400

    Still Debugging Paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,16 +1,15 @@
 class Pin < ActiveRecord::Base
   attr_accessible :description, :image
 
-
   validates :description, presence: true
   validates :user_id, presence: true
-  has_attached_file :image
   
+
   validates_attachment :image, presence: true, 
   														 content_type: {content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']},
   														 size: { less_than: 5.megabytes }
 
   belongs_to :user
-  
+	has_attached_file :image, styles: {medium -> "300x300"}
   
 end
